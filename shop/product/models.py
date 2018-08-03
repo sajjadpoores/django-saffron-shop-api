ae077b4c5bd4e5eaeb3b5c2ef1bb1aa9553571ef
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2018-08-04 01:57:41 +0430

    add Category and Product models to product app

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from account.models import Account
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(verbose_name='دسته', max_length=50, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='نام محصول', max_length=50, blank=False, null=False)
+    photo = models.ImageField(verbose_name='تصویر', upload_to='')
+    price = models.IntegerField(verbose_name='قیمت', blank=False, null=False)
+    description = models.TextField(verbose_name='توضیحات', max_length=500, blank=False, null=False)
+    inventory = models.IntegerField(verbose_name='موجودی', blank=False, null=False, default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='دسته', blank=False, null=False)
+
+    def __str__(self):
+        return self.name
