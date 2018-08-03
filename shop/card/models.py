a470d83d062451fd5e26e97b389e4fe098b904b8
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2018-08-04 02:05:16 +0430

    add Card and CardItem models to card app

--- shop/card/models.py
+++ shop/card/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from account.models import Account
+from product.models import Product
 
-# Create your models here.
+
+class Card(models.Model):
+    client = models.ForeignKey(Account, on_delete=models.CASCADE, verbose_name='مشتری', blank=False, null=False)
+    last_update = models.DateTimeField(verbose_name='آخرین آپدیت', blank=False, null=False, auto_now=True)
+    is_payed = models.BooleanField(verbose_name='وضعیت پرداخت', default=False, blank=False)
+    total = models.PositiveIntegerField(verbose_name='مجموع', default=0, blank=False, null=False)
+
+    def __str__(self):
+        return self.client.__str__() + ' - ' + str(self.last_update)
+
+
+class CardItem(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, verbose_name='سبد خرید', blank=False, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصول', blank=False, null=False)
+    count = models.PositiveIntegerField(verbose_name='تعداد', default=1, blank=False, null=False)
+    create_time = models.DateTimeField(verbose_name='زمان ایجاد', blank=False, null=False, auto_now=True)
+
+    def __str__(self):
+        return self.card + ' - ' + self.product.__str__()
