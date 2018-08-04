364f0baf0d0f19b13ffbac28a1e189ab89d20015
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2018-08-04 22:09:07 +0430

    add Product and Category admin pages

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Product, Category
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category, CategoryAdmin)
