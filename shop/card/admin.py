f410ccc9dd53920765d885c3fed426779223032f
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2018-08-04 22:11:52 +0430

    add Card and CardItem admin pages

--- shop/card/admin.py
+++ shop/card/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Card, CardItem
 # Register your models here.
+
+class CardAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Card, CardAdmin)
+
+class CardItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(CardItem, CardItemAdmin)
